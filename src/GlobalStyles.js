711fa27cff8db647e10ab25f243532c8344cc11e
devrathrana <devrath.rana@bold.com>
2024-02-26 16:16:02 +0530

    add font family using global styles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,12 +1,12 @@
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
-const GlobalStyles=createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
 :root{
   --primary-box:1px solid black
 }
 body {
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  font-family: "Kurale", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,6 +22,6 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
-`
+`;
 
 export default GlobalStyles;
